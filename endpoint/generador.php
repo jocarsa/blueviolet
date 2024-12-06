24f648c925a1ba319090399e10fb971d5d9e0887
jocarsa <info@josevicentecarratala.com>
2024-12-06 12:24:01 +0100

    2

--- endpoint/generador.php
+++ endpoint/generador.php
@@ -1,29 +1,27 @@
 <?php
-
-
-
+$anchura = 64;
 // Create a blank 256x256 image
-$image = imagecreatetruecolor(256, 256);
+$image = imagecreatetruecolor($anchura, $anchura);
 
 // Allocate colors
-$backgroundColor = imagecolorallocate($image, 255,255,255); // Black background
-$roomColor = imagecolorallocate($image, 0, 0, 0);      // Green rooms
-$corridorColor = imagecolorallocate($image, 0,0,0); // White corridors
+$backgroundColor = imagecolorallocate($image, 255, 255, 255); // White background
+$roomColor = imagecolorallocate($image, 0, 0, 0); // Black rooms
+$corridorColor = imagecolorallocate($image, 0, 0, 0); // Black corridors
 
 // Fill the background
 imagefill($image, 0, 0, $backgroundColor);
 
 // Define room and corridor dimensions
-$roomCount = rand(5, 10); // Number of rooms
+$roomCount = rand(15, 30); // Number of rooms
 
 $rooms = [];
 
 // Generate random rooms
 for ($i = 0; $i < $roomCount; $i++) {
-    $roomX = rand(10, 200);
-    $roomY = rand(10, 200);
-    $roomWidth = rand(20, 40);
-    $roomHeight = rand(20, 40);
+    $roomX = rand(10, $anchura-10);
+    $roomY = rand(10, $anchura-10);
+    $roomWidth = rand($anchura/5, $anchura/20);
+    $roomHeight = rand($anchura/5, $anchura/20);
     
     // Draw the room as a filled rectangle
     imagefilledrectangle(
@@ -41,11 +39,13 @@ for ($i = 0; $i < $roomCount; $i++) {
 }
 
 // Draw corridors connecting the rooms
+$corridorThickness = 2; // Thickness of the corridor lines
 for ($i = 1; $i < count($rooms); $i++) {
     $prevRoom = $rooms[$i - 1];
     $currRoom = $rooms[$i];
     
     // Draw a horizontal line
+    imagesetthickness($image, $corridorThickness);
     imageline(
         $image,
         $prevRoom['centerX'], $prevRoom['centerY'],
